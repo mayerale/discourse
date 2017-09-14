1355aa6afced1a7ee2072c5e5f0c544af860aa80
Alexander Mayer <me@alexander-mayer.de>
2017-09-15 01:15:56 +0200

    Permalink Importer for JForum
    Normalization: first version.

--- script/import_scripts/jforum/importers/permalink_importer.rb
+++ script/import_scripts/jforum/importers/permalink_importer.rb
@@ -1,8 +1,8 @@
 module ImportScripts::JForum
   class PermalinkImporter
-    CATEGORY_LINK_NORMALIZATION = '/(viewforum.php\?)(?:.*&)?(f=\d+).*/\1\2'
-    POST_LINK_NORMALIZATION = '/(viewtopic.php\?)(?:.*&)?(p=\d+).*/\1\2'
-    TOPIC_LINK_NORMALIZATION = '/(viewtopic.php\?)(?:.*&)?(t=\d+).*/\1\2'
+    CATEGORY_LINK_NORMALIZATION = '/(forums/show)(/\d+)?(/\d+\.page).*/\1\3'
+    #POST_LINK_NORMALIZATION = '/(posts/list)(/\d+)?(/\d+\.page).*/\1\3'
+    TOPIC_LINK_NORMALIZATION = '/(posts/list)(/\d+)?(/\d+\.page).*/\1\3'
 
     # @param settings [ImportScripts::JForum::PermalinkSettings]
     def initialize(settings)
@@ -14,7 +14,7 @@ module ImportScripts::JForum
       normalizations = normalizations.blank? ? [] : normalizations.split('|')
 
       add_normalization(normalizations, CATEGORY_LINK_NORMALIZATION) if @settings.create_category_links
-      add_normalization(normalizations, POST_LINK_NORMALIZATION) if @settings.create_post_links
+      #add_normalization(normalizations, POST_LINK_NORMALIZATION) if @settings.create_post_links
       add_normalization(normalizations, TOPIC_LINK_NORMALIZATION) if @settings.create_topic_links
 
       SiteSetting.permalink_normalizations = normalizations.join('|')
@@ -23,7 +23,7 @@ module ImportScripts::JForum
     def create_for_category(category, import_id)
       return unless @settings.create_category_links && category
 
-      url = "viewforum.php?f=#{import_id}"
+      url = "/forums/show/#{import_id}.page"
 
       Permalink.create(url: url, category_id: category.id) unless permalink_exists(url)
     end
@@ -31,7 +31,7 @@ module ImportScripts::JForum
     def create_for_topic(topic, import_id)
       return unless @settings.create_topic_links && topic
 
-      url = "viewtopic.php?t=#{import_id}"
+      url = "/posts/list/#{import_id}.page"
 
       Permalink.create(url: url, topic_id: topic.id) unless permalink_exists(url)
     end
@@ -39,7 +39,7 @@ module ImportScripts::JForum
     def create_for_post(post, import_id)
       return unless @settings.create_topic_links && post
 
-      url = "viewtopic.php?p=#{import_id}"
+      url = "/posts/list/#{post.topic.id}.page\##{import_id}"
 
       Permalink.create(url: url, post_id: post.id) unless permalink_exists(url)
     end
