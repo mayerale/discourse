e928aeed4222e38ea31d27137a0aed42f8dee7b3
Alexander Mayer <me@alexander-mayer.de>
2017-09-23 18:16:46 +0200

    Remove unused constants, add bookmark types

--- script/import_scripts/jforum/support/constants.rb
+++ script/import_scripts/jforum/support/constants.rb
@@ -6,9 +6,11 @@ module ImportScripts::JForum
     #INACTIVE_MANUAL = 3 # Account deactivated by administrator
     #INACTIVE_REMIND = 4 # Forced user account reactivation
 
+    # TODO morn provide settings
     GROUP_ADMINISTRATORS = 'Administration'
     GROUP_MODERATORS = 'Moderatoren'
 
+    # TODO morn check all user types
     USER_TYPE_NORMAL = 0
     USER_TYPE_INACTIVE = 1
     USER_TYPE_IGNORE = 2
@@ -17,16 +19,12 @@ module ImportScripts::JForum
     AVATAR_TYPE_UPLOADED = 0
     AVATAR_TYPE_REMOTE = 1
 
-    FORUM_TYPE_CATEGORY = 0
-    FORUM_TYPE_POST = 1
-    FORUM_TYPE_LINK = 2
+    TOPIC_NORMAL = 0
+    TOPIC_STICKY = 1
+    TOPIC_ANNOUNCE = 2
 
-    TOPIC_UNLOCKED = 0
-    TOPIC_LOCKED = 1
-    TOPIC_MOVED = 2
-
-    POST_NORMAL = 0
-    POST_STICKY = 1
-    POST_ANNOUNCE = 2
+    BOOKMARK_TYPE_FORUM = 1
+    BOOKMARK_TYPE_TOPIC = 2
+    BOOKMARK_TYPE_USER = 3
   end
 end
